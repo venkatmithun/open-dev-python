663f6f13fd92c3ec13b170cd92a18393dd02b52a
venkatmithun <venkatmithun.k@gmail.com>
2025-06-18 00:52:06 -0700

    cleaning

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from utils.api_helpers import fetch_data
-import pandas as pd
-
-
 def main():
-    url = "https://jsonplaceholder.typicode.com/posts"
-    data = fetch_data(url)
-    df = pd.DataFrame(data)
-    print(df.head())
+    print("...running main...")
 
 
 if __name__ == "__main__":
