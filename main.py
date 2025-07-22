7ffdbc07210e704fab127cc809a67d7a3fcc041b
venkatmithun <41567130+venkatmithun@users.noreply.github.com>
2025-07-22 12:54:26 -0700

    Clean configs and update documentation (#1)

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def main():
+def main() -> None:
+    """Example entry point for the project."""
+    print("Running main.py")
+    print("Fetching and analyzing data...")
     print("...running main...")
+    print("Done.")
 
 
 if __name__ == "__main__":
-    print("Running main.py")
-    print("Fetching and analyzing data...")
     main()
-    print("Done.")
